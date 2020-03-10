3011dd080d73ce5d4fbe7844759ca994d72faa8e
aslamrifai <rifai.aslam@students.amikom.ac.id>
2020-03-10 10:16:00 +0700

    perubahan pengurangan

--- claculator.cs
+++ claculator.cs
@@ -2,10 +2,16 @@ class calculator
 {
 static void Main (string[] args)
 	}
-	
+	{
 	static int penambahan (int a,int b)
 	{
 		return a + b;
+	
+	}
+	
+	static int pengurangan (int a,int b)
+	{
+		return a - b;
 	
 	}
 }
